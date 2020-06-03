d2599ed1a8a58716bfca4f144adf1c4dc2c8e63a
jtluo05 <jtluo05@gmail.com>
2020-06-02 19:56:34 -0700

    Create FishingContest.java

---
+++ src/practice/FishingContest.java
@@ -0,0 +1,22 @@
+package practice;
+
+
+public class FishingContest {
+
+	public FishingContest(HashMap<String, double[]> allCatches) {
+	
+	}
+
+	public double findBiggestFish(String string) {
+	
+		return 0;
+	}
+
+	public  Object findWinner() {
+		
+		return null;
+	}
+
+	
+
+}
